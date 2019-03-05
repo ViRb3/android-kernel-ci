a4c4a6cb5f5c9753cbbe1390229dd06f1b322ae8
Victor <virb3e@gmail.com>
2019-03-05 16:20:13 +0000

    Add prepare script start/end messages

--- prepare.sh
+++ prepare.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
+echo "Installing required packages ..."
+
 apt update > /dev/null || exit "$?"
 
 # required
 apt install git make build-essential ccache zip -y > /dev/null || exit "$?"
 # per kernel
 apt install curl bc libssl-dev python -y > /dev/null || exit "$?"
+
+echo "Done!"
