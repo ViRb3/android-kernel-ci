f12870603510c5a6573a40192186ea2799a85cbd
Victor <virb3e@gmail.com>
2019-03-05 16:29:45 +0000

    Print CI variables on job start

---
+++ print-vars.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+echo
+echo "================================================="
+echo "CI VARIABLES"
+echo "================================================="
+echo "AKCI_REF: ${AKCI_REF}"
+echo "AKCI_LABEL: ${AKCI_LABEL}"
+echo "AKCI_CCACHE: ${AKCI_CCACHE}"
+echo "================================================="
+echo
